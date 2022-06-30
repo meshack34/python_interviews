4e8aa4fb3dd2f45822198f6e307caa179ebbddfb
meshack34 <meshackkimutai34@gmail.com>
2022-06-30 11:23:38 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+def tri_recursion(k):
+    if(k > 0):
+     result = k+k
+     print(result)
+    else:
+     result = 0+4
+     print(result)
+    return result # Return after all execution
+print("Recursion Example Results")
+tri_recursion(2) #call
